11756ccc079816595c34301098a6f586337bd56e
warsa57544 <warsameabdullahi209@gmail.com>
2024-08-13 23:39:53 -0500

    middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = authenticateToken;
