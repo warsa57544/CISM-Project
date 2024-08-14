c60c3fe0663a8b7c261a132e5dd8ad6a2345737a
warsa57544 <warsameabdullahi209@gmail.com>
2024-08-14 15:06:21 -0500

    front end components

---
+++ frontend_prototype/App.js
@@ -0,0 +1,60 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import HomePage from "./screens/HomePage";
+import Dashboard from "./screens/Dashboard";
+import AdminDashboard from "./screens/AdminDashboard";
+import Profiles from "./screens/Profiles";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-BoldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="HomePage"
+              component={HomePage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Dashboard"
+              component={Dashboard}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="AdminDashboard"
+              component={AdminDashboard}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Profiles"
+              component={Profiles}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
---
+++ frontend_prototype/GlobalStyles.js
@@ -0,0 +1,51 @@
+/* fonts */
+export const FontFamily = {
+  robotoRegular: "Roboto-Regular",
+  robotoBold: "Roboto-Bold",
+  montserratSemiBold: "Montserrat-SemiBold",
+  montserratMedium: "Montserrat-Medium",
+  robotoLight: "Roboto-Light",
+  robotoBoldItalic: "Roboto-BoldItalic",
+};
+/* font sizes */
+export const FontSize = {
+  size_7xs: 6,
+  size_xs: 12,
+  size_mini: 15,
+  size_lg: 18,
+  size_4xl: 23,
+  size_xl: 20,
+  size_sm: 14,
+  size_base: 16,
+  size_11xl: 30,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorBlack: "#000",
+  colorLimegreen_100: "#30ac04",
+  colorLimegreen_200: "rgba(4, 206, 0, 0.97)",
+  colorPaleturquoise: "#b2fcfb",
+  colorWhitesmoke_100: "#efeeee",
+  colorRoyalblue: "#2b62ee",
+  colorMediumblue: "#0931ff",
+  colorSkyblue: "#a3d0f0",
+  colorLightgreen: "#94da93",
+  colorDarkslategray: "#102e44",
+  m3SysLightSurfaceContainerHigh: "#ece6f0",
+};
+/* Paddings */
+export const Padding = {
+  p_base: 16,
+  p_9xs: 4,
+  p_11xl: 30,
+};
+/* border radiuses */
+export const Border = {
+  br_11xl: 30,
+  br_11xs: 2,
+  br_3xs: 10,
+  br_xl: 20,
+  br_9xl: 28,
+  br_21xl: 40,
+};
